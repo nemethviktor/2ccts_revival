2f3629a2071ba7aba9d093484822a9dc7f3e65b4
Viktor Nemeth <58767235+nemethviktor@users.noreply.github.com>
2026-03-24 13:33:34 +0000

    Reclassify gauges for non-electric

--- pybuild.bat
+++ pybuild.bat
@@ -3,7 +3,7 @@ cls
 python ./tools/custom_tags_generator.py
 python ./tools/check_src_files_exist.py
 python ./tools/data_to_property.py
-echo "--- Build Start ---"
+echo --- Build Start ---
 gcc -E -x c -o 2ccts_revival.nml ./2ccts_revival.pnml
 nmlc -c --quiet 2ccts_revival.nml -o "2ccts_revival.grf" -t custom_tags.txt
-echo "--- Build Finished ---"
+echo --- Build Finished ---
